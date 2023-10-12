8469d4cd752d8b2219a41c115f186d5116ec9760
Julia Georgiew <java2011@me.com>
2023-10-12 20:20:19 +0200

    rename todobackend

--- todobackend/asgi.py
+++ todobackend/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for todolistbackend project.
+ASGI config for todobackend project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todolistbackend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todobackend.settings')
 
 application = get_asgi_application()
